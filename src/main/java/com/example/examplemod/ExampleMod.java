78db661339b76d57ce29da267a8a3bf2fcc8fa66
Tomassy <87383589+Tomassy69@users.noreply.github.com>
2023-06-01 08:31:32 -0400

    yay something

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -8,8 +8,8 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "bap_utils";
+    public static final String VERSION = "beta 0.1";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
