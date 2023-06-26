9e94eb2ab6bf72710e83a1db9ef1ff4602bcedcc
Jerry <jerryfu_mc@outlook.com>
2023-06-26 16:03:35 -0400

    added takeover command!

---
+++ src/main/java/net/jerrydev/baputils/gui/JavaTestGui.java
@@ -0,0 +1,37 @@
+package net.jerrydev.baputils.gui;
+
+
+import gg.essential.elementa.ElementaVersion;
+import gg.essential.elementa.UIComponent;
+import gg.essential.elementa.WindowScreen;
+import gg.essential.elementa.components.UIBlock;
+import gg.essential.elementa.constraints.CenterConstraint;
+import gg.essential.elementa.constraints.ChildBasedSizeConstraint;
+import gg.essential.elementa.constraints.PixelConstraint;
+import gg.essential.elementa.constraints.animation.AnimatingConstraints;
+import gg.essential.elementa.constraints.animation.Animations;
+import gg.essential.elementa.effects.ScissorEffect;
+
+public class JavaTestGui extends WindowScreen {
+    UIComponent box = new UIBlock()
+            .setX(new CenterConstraint())
+            .setY(new PixelConstraint(10f))
+            .setWidth(new PixelConstraint(10f))
+            .setHeight(new PixelConstraint(36f))
+            .setChildOf(getWindow())
+            .enableEffect(new ScissorEffect());
+
+    public JavaTestGui() {
+        super(ElementaVersion.V2);
+        System.out.println("IN TESTGUI");
+        box.onMouseEnterRunnable(() -> {
+            // Animate, set color, etc.
+            AnimatingConstraints anim = box.makeAnimation();
+            anim.setWidthAnimation(Animations.OUT_EXP, 0.5f, new ChildBasedSizeConstraint(2f));
+            anim.onCompleteRunnable(() -> {
+                // Trigger new animation or anything.
+            });
+            box.animateTo(anim);
+        });
+    }
+}
--- src/main/java/net/jerrydev/baputils/listeners/ChatHandler.java
+++ src/main/java/net/jerrydev/baputils/listeners/ChatHandler.java
@@ -1,14 +1,65 @@
 package net.jerrydev.baputils.listeners;
 
+import net.jerrydev.baputils.utils.PtoEncrypt;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.tileentity.TileEntityBeaconRenderer;
+import net.minecraft.entity.Entity;
+import net.minecraft.init.Blocks;
+import net.minecraft.network.play.client.C0APacketAnimation;
+import net.minecraft.tileentity.TileEntityBeacon;
+import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.Thread.sleep;
 
 public class ChatHandler {
     @SubscribeEvent
-    public void ChatHandler(ClientChatReceivedEvent event){
-        if(event.message.getUnformattedText().toLowerCase().contains("hello")){
-            System.out.println(event.message.getUnformattedText());
-            System.out.println("Hello, World!");
+    public void ChatHandler(ClientChatReceivedEvent event) {
+        if (event.message.getUnformattedText().replaceAll("§[0-9a-fk-or]", "").replaceAll("\\[.*?\\\\]\\s", "").matches("(?i)Party > .*: \\$t\\/.*")) {
+            // decrypt
+            String message = event.message.getUnformattedText().replaceAll("§[0-9a-fk-or]", "").replaceAll("\\[.*?]\\s", "");
+
+            int startIndex = message.indexOf("$t/") + 3;
+            String decryptedContent = null;
+            try {
+                decryptedContent = PtoEncrypt.decryptString(message.substring(startIndex), (int) ((System.currentTimeMillis() / 1000) / 2) * 2);
+            } catch (Exception e) {
+                Minecraft.getMinecraft().thePlayer.sendChatMessage("$baputils: Takeover failed: A bad key was used! javax.crypto.BadPaddingException");
+            }
+
+            // Construct the decrypted message with the "$t/" prefix
+            String messageDecrypted = message.substring(0, startIndex) + decryptedContent;
+
+            // match "Party > [any] Any $baputilsptoAny
+            String regExpPattern = "(?i)Party > (.*): \\$t\\/baputils\\/pto\\/"
+                    + Pattern.quote(Minecraft.getMinecraft().thePlayer.getName());
+
+            if (messageDecrypted.matches(regExpPattern)) {
+                Pattern regex = Pattern.compile(regExpPattern, Pattern.CASE_INSENSITIVE);
+                Matcher matcher = regex.matcher(messageDecrypted);
+                if (matcher.matches()) {
+
+                    String playerName = matcher.group(1);
+
+                    // Transfer the party
+                    new Thread(() -> {
+                        try {
+                            Thread.sleep(100);
+                            Minecraft.getMinecraft().thePlayer.sendChatMessage("$baputils: Transferring the party.");
+                            Thread.sleep(100);
+                            Minecraft.getMinecraft().thePlayer.sendChatMessage("/party transfer " + playerName);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Minecraft.getMinecraft().thePlayer.sendChatMessage("$baputils: Takeover failed! An error occurred while transferring the party.");
+                        }
+                    }).start();
+                }
+            }
         }
     }
 }
